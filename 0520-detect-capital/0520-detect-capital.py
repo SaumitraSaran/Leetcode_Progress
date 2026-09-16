ae5d999d2fe254eb99f99f9509aebc29fcc71fa1
Saumitra Saran <saumitra.saran@gmail.com>
2026-09-16 10:55:12 +0530

    Time: 0 ms (100%), Space: 12.4 MB (53.56%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -4,26 +4,27 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        ind = 0
-        if len(word) == 1:
-                return True
+        # ind = 0
+        # if len(word) == 1:
+        #         return True
 
-        if len(word) == 2 and word[0].islower() and word[1].isupper():
-                return False
+        # if len(word) == 2 and word[0].islower() and word[1].isupper():
+        #         return False
 
-        if word[ind].isupper() and word[ind+1].isupper():
-            for i in range(ind+2, len(word)):
-                if word[i].islower():
-                    return False
+        # if word[ind].isupper() and word[ind+1].isupper():
+        #     for i in range(ind+2, len(word)):
+        #         if word[i].islower():
+        #             return False
 
-        if word[ind].isupper() and word[ind+1].islower():
-            for j in range(ind+2, len(word)):
-                if word[j].isupper():
-                    return False
+        # if word[ind].isupper() and word[ind+1].islower():
+        #     for j in range(ind+2, len(word)):
+        #         if word[j].isupper():
+        #             return False
 
-        if word[ind].islower():
-            for k in range(ind+1, len(word)):
-                if word[k].isupper():
-                    return False
+        # if word[ind].islower():
+        #     for k in range(ind+1, len(word)):
+        #         if word[k].isupper():
+        #             return False
 
-        return True
+        # return True
+        return word.isupper() or word.islower() or word.istitle()
