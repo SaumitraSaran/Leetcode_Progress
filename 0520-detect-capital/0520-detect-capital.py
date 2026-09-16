b242dfbef13cbe27e2c641a66d6e6255ef89a166
Saumitra Saran <saumitra.saran@gmail.com>
2026-09-16 10:52:32 +0530

    Time: 0 ms (100%), Space: 12.4 MB (16.54%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        ind = 0
+        if len(word) == 1:
+                return True
+
+        if len(word) <= 2 and word[0].islower() and word[1].isupper():
+                return False
+
+        if word[ind].isupper() and word[ind+1].isupper():
+            for i in range(ind+2, len(word)):
+                if word[i].islower():
+                    return False
+
+        if word[ind].isupper() and word[ind+1].islower():
+            for j in range(ind+2, len(word)):
+                if word[j].isupper():
+                    return False
+
+        if word[ind].islower():
+            for k in range(ind+1, len(word)):
+                if word[k].isupper():
+                    return False
+
+        return True
